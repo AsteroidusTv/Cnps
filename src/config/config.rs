5a3ac22822b4af893f8ad48b92d0f80143420ab0
AstroidusTv <asteroidus@protonmail.com>
2023-07-29 16:32:53 +0300

    clean code

--- src/config/config.rs
+++ src/config/config.rs
@@ -5,12 +5,6 @@ use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 
-#[derive(Serialize, Deserialize)]
-struct Configuration {
-    found_path: String,
-    editor: String,
-}
-
 fn ask_string(message: &str) -> String {
     println!("{}", message);
     let stdin = io::stdin();
@@ -32,8 +26,8 @@ fn find_directory(start_path: &Path, target_directory1: &str, target_directory2:
         if path.is_dir() {
             if path.ends_with(target_directory1) || path.ends_with(target_directory2) {
                 return Some(path);
-            } else if let Some(found_path) = find_directory(&path, target_directory1, target_directory2) {
-                return Some(found_path);
+            } else if let Some(folder_path) = find_directory(&path, target_directory1, target_directory2) {
+                return Some(folder_path);
             }
         }
     }
@@ -41,57 +35,26 @@ fn find_directory(start_path: &Path, target_directory1: &str, target_directory2:
     None
 }
 
-fn add_to_json(found_path: &str, editor: &str) {
-    // Create an instance of Configuration with the provided values
-    let config = Configuration {
-        found_path: found_path.to_string(),
-        editor: editor.to_string(),
-    };
-
-    // Convert the structure to JSON
-    let serialized_config = serde_json::to_string(&config).unwrap();
-
-    // Specify the path to the configuration file
-    let file_path = "config.json";
-
-    // Write the data to the configuration file
-    let mut file = File::create(file_path).expect("Failed to create the file");
-    file.write_all(serialized_config.as_bytes())
-        .expect("Failed to write to the file");
-
-    // Load the data from the configuration file
-    let mut file = File::open(file_path).expect("Failed to open the file");
-    let mut content = String::new();
-    file.read_to_string(&mut content)
-        .expect("Failed to read from the file");
-
-    // Deserialize the JSON data into the Configuration struct
-    let deserialized_config: Configuration =
-        serde_json::from_str(&content).expect("Failed to deserialize the data");
-
-    // Use the deserialized_config as needed
-    println!("Found Path: {}", deserialized_config.found_path);
-    println!("Editor: {}", deserialized_config.editor);
-}
 
 pub fn main() {
+
     let start_path = Path::new("/home/");
     let target_directory1 = "projects";
     let target_directory2 = "Projects";
 
-    if let Some(found_path) = find_directory(start_path, target_directory1, target_directory2) {
-        println!("Found directory: {:?}", found_path);
-        let folder_response = ask_string(format!("Is {:?} your project folder ? yN:", found_path).as_str()).to_lowercase();
+    if let Some(folder_path) = find_directory(start_path, target_directory1, target_directory2) {
+        let folder_response = ask_string(format!("Is {:?} your project folder ? yN:", folder_path).as_str()).to_lowercase();
         if folder_response == "y" {
-            add_to_json(found_path.to_str().unwrap(), "code");
+            // Add found_path to json
         }
         else {
-            let folder_response = ask_string("Enter your projects folder path : ");
+            let _folder_path = ask_string("Enter your projects folder path : ");
+            // Add entered_path to json
         }
     }
     else {
-        let folder_response = ask_string("Enter your projects folder path: ");
-        add_to_json(&folder_response, "");
+        let _folder_path = ask_string("Enter your projects folder path: ");
+        // Add entered_path to json
     }
 
     loop {
@@ -99,8 +62,10 @@ pub fn main() {
         if editor_response != "subl" && editor_response != "code" {
             println!("Invalid IDE response: {} you must choose 'subl' or 'code'", editor_response);
         }
-            break;
+        else {
+            // Add editor response to json
         }
     }
+}
     
 
