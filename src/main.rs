f06e48c14e3ae576e0f006d511dae372a0968255
AstroidusTv <asteroidus@protonmail.com>
2023-07-30 17:08:11 +0300

    Add subfolder feature !

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ mod config {
 }
 #[derive(Serialize, Deserialize)]
 struct Data {
+
     folder_path: PathBuf,
     editor_response: String,
 }
@@ -51,6 +52,45 @@ fn change_directory(dir: &str) {
     let documents_dir = env::current_dir().unwrap().join(dir);
     env::set_current_dir(&documents_dir).expect("Imposible to set documents directory");
 }
+fn get_project_folders(paths: &str) -> Option<String> {
+    let paths = fs::read_dir(paths).unwrap();
+    let mut in_choices: Vec<String> = Vec::new();
+
+    for path in paths {
+        let entry = path.unwrap();
+        let path = entry.path();
+        let path_str = path.to_str().unwrap().to_string();
+        let path_components: Vec<&str> = path_str.split('/').collect();
+        if let Some(last_folder) = path_components.last() {
+            in_choices.push(last_folder.to_string());
+        }
+    }
+
+    let choices: &Vec<String> = &in_choices;
+    let mut choice = "";
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Subfolder to use")
+        .items(choices)
+        .default(0)
+        .interact()
+        .unwrap();
+
+    for i in 0..in_choices.len() {
+        if i == selection {
+            choice = &in_choices[i];
+            break;
+        }
+    }
+
+    if !choice.is_empty() {
+        Some(choice.to_string())
+    } else {
+        None
+    }
+
+}
+
 
 fn main() {
 
@@ -64,18 +104,41 @@ fn main() {
     file.read_to_string(&mut data).expect("Failed to read the file.");
     let data: Data = serde_json::from_str(&data).expect("Failed to deserialize JSON.");
 
-    // Command to execute
-    let git_command = "git";
-    let git_args = vec!["init"];
-    let editor_command = data.editor_response.as_str();
-    let editor_args = vec!["."];
-
     // Change main directory to folder path from json file
     let folder_str = data.folder_path.to_str().unwrap_or("default_folder_path");
     change_directory(folder_str);
 
     // Ask for project name
     let project_name = ask_string("Please enter the project name");
+
+    let subfolder_response = ask_string("Would you like to create your project in a subfolder ?: y/n").to_lowercase();
+
+    let open_args: Vec<String>; // Déclarez open_args en dehors du bloc if-else
+
+    if subfolder_response == "y" {
+        if let Some(choice) = get_project_folders("/home/achille/Documents/Projects") {
+            change_directory(choice.as_str());
+            println!("Selected choice: {}", choice);
+            open_args = vec![format!("{}/{}/{}/index.html", folder_str, choice, project_name)];
+        } else {
+            println!("No choice selected.");
+            open_args = vec![format!("{}/{}/index.html", folder_str, project_name)];
+    }
+    } else {
+        open_args = vec![format!("{}/{}/index.html", folder_str, project_name)];
+    }
+     
+    // Command to execute
+    let git_command = "git";
+    let git_args = vec!["init"];
+    let editor_command = data.editor_response.as_str();
+    let editor_args = vec!["."];
+    let open_command = "open";
+    // For open args go to ligne 120
+    
+
+    // Convert
+    let open_args_str: Vec<&str> = open_args.iter().map(|arg| arg.as_str()).collect();
 
     loop {
         
@@ -107,6 +170,7 @@ fn main() {
             change_directory(project_name.as_str());
             command_execute(git_command, git_args);
             command_execute(editor_command, editor_args);
+            command_execute(open_command, open_args_str);
             break;
 
         } else if choice == "go" {
